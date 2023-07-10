df62a1e9818dcde56ce6a494b8ae7e5993eaca9b
bernardo silva <bernardovs2003@gmail.com>
2023-07-10 12:15:13 +0100

    Add files via upload

---
+++ q.ps1
@@ -0,0 +1,56 @@
+
+Function Help {
+
+    param(
+        [bool]$fromHelp
+    )
+
+    Write-Host "";
+
+    if($fromHelp){
+        Write-Host " Need help?" -ForegroundColor Blue -NoNewLine;
+    }else{
+        Write-Host " Unknown command!" -ForegroundColor Red -NoNewLine;
+    }
+    Write-Host " Avaiable commands:";
+
+    HelpCommand "help" "View avaiable commands";
+    HelpCommand "edit" "Edit q on VS Code";
+    HelpCommand "mix" "Run laravel-mix in watch mode";
+    HelpCommand "reset-db" "Reset Laravel database";
+
+    Write-Host "";
+
+}
+
+Function HelpCommand {
+
+    param(
+        [string]$command,
+        [string]$description
+    )
+
+    Write-Host " q" -ForegroundColor Yellow -NoNewLine;
+    
+    Write-Host " $command" -NoNewLine;
+    Write-Host " - $description" -ForegroundColor DarkGray;
+}
+
+switch($args[0]){
+    "help" {
+        Help $true;
+    }
+    "edit" {
+        code "$PSScriptRoot\q.ps1";
+    }
+    "mix" {
+        npx mix watch
+    }
+    "reset-db" {
+        php artisan migrate:fresh --seed
+        break;
+    }
+    default {
+        Help $false;
+    }
+}
